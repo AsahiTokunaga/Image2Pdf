5ad8ad08143078fb9d0d9aa600dbcac16ec8c46a
hihimamuLab <hihimamug14@gmail.com>
2024-11-07 17:36:18 +0900

    Print Progress

--- src/main.rs
+++ src/main.rs
@@ -1,113 +1,100 @@
-use std::fs;
-use std::fs::File;
-use std::io::BufWriter;
-use std::path::Path;
-use std::env;
-
 use anyhow::{Ok, Result};
 use image::{DynamicImage, ImageFormat};
 use jpeg_to_pdf::JpegToPdf;
+use std::fs::File;
+use std::io::BufWriter;
+use std::path::Path;
+use std::{env, fs};
 use walkdir::{DirEntry, WalkDir};
 
-const ALLOW_EXTENSIONS: [&str; 6] = ["jpg", "jpeg", "JPG", "JPEG", "png", "PNG"];
+const ALLOW_EXTENSIONS: [&str; 4] = ["jpg", "jpeg", "png", "avif"];
 
 fn main() -> Result<()> {
     for args in env::args().skip(1) {
-        image2pdf(&args)?;   
+        to_pdf(&args)?;
     }
     Ok(())
 }
 
-fn image2pdf<P: AsRef<Path> + ToString + Copy>(root: P) -> Result<()> {
+fn to_pdf<P: AsRef<Path>>(path: &P) -> Result<()> {
+    let paths: Vec<DirEntry> = WalkDir::new(path)
+        .sort_by_file_name()
+        .contents_first(true)
+        .into_iter()
+        .filter_map(|entry| entry.ok())
+        .collect();
     let mut progress: u32 = 0;
+    let file_count: u32 = get_file_count(path)?;
     let mut images: Vec<Vec<u8>> = Vec::new();
-    for entry in WalkDir::new(root).sort_by_file_name().contents_first(true) {
-        let entry: DirEntry = entry?;
-        if entry
-            .path()
-            .parent()
-            .unwrap()
-            .with_extension("pdf")
-            .as_path()
-            .is_file()
-        {
-            continue;
-        }
+    for entry in paths {
         if entry.path().is_dir() {
             if images.is_empty() {
                 continue;
             }
-            let pdf: File = File::create(format!("{}.pdf", entry.path().to_str().unwrap()))?;
-            match JpegToPdf::new()
+            let pdf: File = File::create(entry.path().with_extension("pdf"))?;
+            JpegToPdf::new()
                 .add_images(images.clone())
-                .create_pdf(&mut BufWriter::new(pdf))
-            {
-                std::result::Result::Ok(_) => {
-                    println!(
-                        "[Done] Created PDF: {}",
-                        format!("{}.pdf", entry.path().to_str().unwrap())
-                    );
-                    images.clear();
-                    progress = 0;
-                    continue;
-                }
-                Err(e) => {
-                    println!(
-                        "[FAILED] Couldn't Create PDF: {}",
-                        format!("{}.pdf", entry.path().to_str().unwrap())
-                    );
-                    println!("{}", e);
-                    images.clear();
-                    progress = 0;
-                    continue;
-                }
-            }
+                .create_pdf(&mut BufWriter::new(pdf))?;
+            println!(
+                "[Done] Created PDF: {}",
+                entry.path().with_extension("pdf").display()
+            );
+            images.clear();
+            continue;
         }
-        let extension: &str = entry.path().extension().unwrap().to_str().unwrap();
-        if ALLOW_EXTENSIONS.iter().any(|&ex| ex == extension) {
-            let all_files: u32 = get_file_count(entry.path().parent().unwrap().to_str().unwrap())?;
-            if ["png", "PNG"]
-                .iter()
-                .any(|&ex| ex == entry.path().extension().unwrap())
-            {
-                png_to_jpg(entry.path())?;
+        if ALLOW_EXTENSIONS
+            .into_iter()
+            .any(|ex| ex.eq_ignore_ascii_case(entry.path().extension().unwrap().to_str().unwrap()))
+        {
+            if ["png", "avif"].iter().any(|&ex| {
+                ex.eq_ignore_ascii_case(entry.path().extension().unwrap().to_str().unwrap())
+            }) {
+                to_jpg(entry.path())?;
                 images.push(fs::read(entry.path().with_extension("jpg"))?);
-                progress += 1;
                 println!(
-                    "[{}/{}] Added Image: {}",
+                    "[{}/{} Progress: {}%] Added Image: {}",
                     progress,
-                    all_files,
-                    entry.path().to_str().unwrap()
+                    file_count,
+                    ((progress as f64 / file_count as f64) * 100.0) as u32,
+                    entry.path().display()
                 );
+                progress += 1;
                 continue;
             }
             images.push(fs::read(entry.path())?);
-            progress += 1;
             println!(
-                "[{}/{}] Added Image: {}",
+                "[{}/{} Progress: {}%] Added Image: {}",
                 progress,
-                all_files,
+                file_count,
+                ((progress as f64 / file_count as f64) * 100.0) as u32,
                 entry.path().display()
             );
+            progress += 1;
         }
     }
+    Ok(())
+}
+
+fn to_jpg<P: AsRef<Path>>(path: P) -> Result<()> {
+    let image: DynamicImage = image::open(&path)?;
+    image.save_with_format(path.as_ref().with_extension("jpg"), ImageFormat::Jpeg)?;
+    fs::remove_file(path)?;
     Ok(())
 }
 
 fn get_file_count<P: AsRef<Path>>(path: P) -> Result<u32> {
-    let mut file_count: u32 = 0;
-    for entry in WalkDir::new(path) {
-        let entry: DirEntry = entry?;
-        if entry.file_type().is_file() {
-            file_count += 1;
-        }
+    let mut count: u32 = 0;
+    for _ in WalkDir::new(path)
+        .into_iter()
+        .filter_map(|entry| entry.ok())
+        .filter(|entry| {
+            entry.path().is_file()
+                && ALLOW_EXTENSIONS.iter().any(|&ex| {
+                    ex.eq_ignore_ascii_case(entry.path().extension().unwrap().to_str().unwrap())
+                })
+        })
+    {
+        count += 1;
     }
-    Ok(file_count)
-}
-
-fn png_to_jpg<P: AsRef<Path> + Copy>(path: P) -> Result<()> {
-    let image: DynamicImage = image::open(path)?;
-    image.save_with_format(path.as_ref().with_extension("jpg"), ImageFormat::Jpeg)?;
-    fs::remove_file(path)?;
-    Ok(())
+    Ok(count)
 }
