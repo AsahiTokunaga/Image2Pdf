029b9f6a7e9b0cbb931a82ad6d9e5abd6e0075f8
hihimamuLab <hihimamug14@gmail.com>
2024-11-09 01:18:28 +0900

    Fix function to convert png from jpg

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use anyhow::{Ok, Result};
-use image::{DynamicImage, ImageFormat};
+use image::{ImageFormat, RgbImage};
 use jpeg_to_pdf::JpegToPdf;
 use std::fs::File;
 use std::io::BufWriter;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::{env, fs};
 use walkdir::{DirEntry, WalkDir};
 
@@ -23,52 +23,44 @@ fn to_pdf<P: AsRef<Path>>(path: &P) -> Result<()> {
         .into_iter()
         .filter_map(|entry| entry.ok())
         .collect();
-    let mut progress: u32 = 0;
+    let mut progress: u32 = 1;
     let file_count: u32 = get_file_count(path)?;
     let mut images: Vec<Vec<u8>> = Vec::new();
     for entry in paths {
+        if entry.path().is_dir() && images.is_empty() {
+            continue;
+        }
         if entry.path().is_dir() {
-            if images.is_empty() {
-                continue;
-            }
             let pdf: File = File::create(entry.path().with_extension("pdf"))?;
-            JpegToPdf::new()
+            match JpegToPdf::new()
                 .add_images(images.clone())
-                .create_pdf(&mut BufWriter::new(pdf))?;
-            println!(
-                "[Done] Created PDF: {}",
-                entry.path().with_extension("pdf").display()
-            );
+                .create_pdf(&mut BufWriter::new(pdf)) {
+                std::result::Result::Ok(_) => println!(
+                    "[Done] Created PDF: {}",
+                    entry.path().with_extension("pdf").display()
+                ),
+                Err(e) => {
+                    println!(
+                        "[FAILED] Couldn't Create PDF: {}",
+                        entry.path().with_extension("pdf").display()
+                    );
+                    println!("{}", e);
+                }
+            }
             images.clear();
             continue;
         }
-        if ALLOW_EXTENSIONS
-            .into_iter()
-            .any(|ex| ex.eq_ignore_ascii_case(entry.path().extension().unwrap().to_str().unwrap()))
-        {
-            if ["png", "avif"].iter().any(|&ex| {
-                ex.eq_ignore_ascii_case(entry.path().extension().unwrap().to_str().unwrap())
-            }) {
+        let extention: String = entry.path().extension().unwrap().to_str().unwrap().to_lowercase();
+        if ALLOW_EXTENSIONS.iter().any(|&ex| ex == extention) {
+            let image_path: PathBuf = if ["png", "avif"].contains(&extention.as_str()) {
                 to_jpg(entry.path())?;
-                images.push(fs::read(entry.path().with_extension("jpg"))?);
-                println!(
-                    "[{}/{} Progress: {}%] Added Image: {}",
-                    progress,
-                    file_count,
-                    ((progress as f64 / file_count as f64) * 100.0) as u32,
-                    entry.path().display()
-                );
-                progress += 1;
-                continue;
-            }
-            images.push(fs::read(entry.path())?);
-            println!(
-                "[{}/{} Progress: {}%] Added Image: {}",
-                progress,
-                file_count,
-                ((progress as f64 / file_count as f64) * 100.0) as u32,
-                entry.path().display()
-            );
+                entry.path().with_extension("jpg").to_path_buf()
+            } else {
+                entry.path().to_path_buf()
+            };
+            let image: Vec<u8> = fs::read(&image_path)?;
+            images.push(image);
+            println!("[{}/{}] Added Image: {}", progress, file_count, image_path.display());
             progress += 1;
         }
     }
@@ -76,7 +68,7 @@ fn to_pdf<P: AsRef<Path>>(path: &P) -> Result<()> {
 }
 
 fn to_jpg<P: AsRef<Path>>(path: P) -> Result<()> {
-    let image: DynamicImage = image::open(&path)?;
+    let image: RgbImage = image::open(&path)?.to_rgb8();
     image.save_with_format(path.as_ref().with_extension("jpg"), ImageFormat::Jpeg)?;
     fs::remove_file(path)?;
     Ok(())
