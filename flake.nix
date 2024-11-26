3d5ecf9f02e4807009101cf8ffae6e03b5e04457
hihimamuLab <hihimamug14@gmail.com>
2024-11-26 21:03:52 +0900

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Image2Pdf cargo project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-stable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: {
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        inherit (nixpkgs.lib) optional;
+        pkgs = import nixpkgs { inherit system; };
+
+        rustup = pkgs.rustup;
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            rustup
+          ];
+        };
+      }
+    );
+  };
+}
